c6804bc647242e93aec6ebf35a199346c0949cf5
Tasha <ncarr63@gmail.com>
2022-12-20 21:11:39 +1300

    find why external api isn't working

--- src/services/API.js
+++ src/services/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export default(url='https://jsonplaceholder.typicode.com/todos/') => {
+export default(url='https://jsonplaceholder.typicode.com/todos') => {
     return axios.create({
         baseURL: url,
     }
